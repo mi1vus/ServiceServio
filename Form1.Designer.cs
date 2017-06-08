8a1df4a69a43c8fc7c6910b6b8a0e251794168cd
Max <Max@DESKTOP-UGEV1NB>
2017-06-08 17:59:48 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.operatorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.operatorBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(417, 186);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(330, 27);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Оператор:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(330, 147);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // operatorBindingSource
+            // 
+            this.operatorBindingSource.DataSource = typeof(Service.Form1.Operator);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.Location = new System.Drawing.Point(339, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сервис";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 186);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(280, 225);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.operatorBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource operatorBindingSource;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using ServioPumpGAS_Driver;
+
+namespace Service
+{
+    public partial class Form1 : Form
+    {
+        [XmlRoot("terminaldictionary")]
+        public class Dictionary
+        {
+            [XmlElement("operators")]
+            public List<Operator> Operators { get; set; }
+            [XmlElement("azses")]
+            public List<AZS> AZSes { get; set; }
+            [XmlElement("terminals")]
+            public List<Terminal> Terminals { get; set; }
+        }
+        public class Operator
+        {
+            [XmlElement("id")]
+            public int Id { get; set; }
+            [XmlElement("name")]
+            public string Name { get; set; }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+        public class AZS
+        {
+            [XmlElement("id")]
+            public int Id { get; set; }
+            [XmlElement("name")]
+            public string Name { get; set; }
+            [XmlElement("idoperator")]
+            public int IdOperator { get; set; }
+        }
+        public class Terminal
+        {
+            [XmlElement("id")]
+            public int Id { get; set; }
+            [XmlElement("ip")]
+            public string Ip { get; set; }
+            [XmlElement("name")]
+            public string Name { get; set; }
+            [XmlElement("idazs")]
+            public int IdAZS { get; set; }
+        }
+
+        private static bool isValidXml(string candidate)
+        {
+            try
+            {
+                XElement.Parse(candidate);
+            }
+            catch (XmlException) { return false; }
+            return true;
+        }
+
+        private static List<Терминал> ReadXml(int operatorId = -1)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Dictionary));
+            var settFile = File.ReadAllText("Terminals.xml");
+            if (!isValidXml(settFile))
+                return new List<Терминал>();
+
+            using (TextReader reader = new StringReader(settFile))
+            {
+                Dictionary result = (Dictionary)serializer.Deserialize(reader);
+
+                var azses = result.Operators.Join(
+                    result.AZSes,
+                    oper => oper.Id,
+                    azs => azs.IdOperator,
+                    (oper, azs) => new
+                    {
+                        azsid = azs.Id,
+                        azs = azs.Name,
+                        oper = oper.Name,
+                        operId = oper.Id
+                    }
+                );
+                var r1 = azses.ToList();
+
+                var terminals = r1.Join(
+                    result.Terminals,
+                    azs => azs.azsid,
+                    term => term.IdAZS,
+                    (azs, term) => new Терминал(
+                            term.Id,
+                            azs.azs,
+                            term.Name,
+                            azs.oper,
+                            azs.operId,
+                            term.Ip
+                        )
+                );
+                if (operatorId <= 0)
+                    return terminals.ToList();
+                else
+                    return terminals.Where(t=>t.ОператорId == operatorId).ToList();
+            }
+
+        }
+
+        class Терминал
+        {
+            public int Id;//Данное свойство не будет отображаться как колонка
+            public string Название { get; set; } //обязательно нужно использовать get конструкцию
+            public string АЗС { get; set; }
+            public string Оператор { get; set; }
+            public int ОператорId; //Данное свойство не будет отображаться как колонка
+            public string IP { get; set; }
+
+            public Терминал(int id, string азс, string название, string оператор, int операторId, string ip)
+            {
+                this.Id = id;
+                this.АЗС = азс;
+                this.Название = название;
+                this.Оператор = оператор;
+                this.ОператорId = операторId;
+                this.IP = ip;
+            }
+
+            public override string ToString()
+            {
+                return $"[{IP}] {Название} - {Оператор}";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var data = dataGridView1.DataSource as BindingList<Терминал>;
+            data.Clear();
+            foreach (var terminal in ReadXml((comboBox1.SelectedItem as Operator).Id))
+                (dataGridView1.DataSource as BindingList<Терминал>).Add(terminal);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindingList<Терминал> data = new BindingList<Терминал>(); //Специальный список List с вызовом события обновления внутреннего состояния, необходимого для автообновления datagridview
+            dataGridView1.DataSource = data;
+
+            //ServioPumpGAS_Driver.Driver.Service(new Dictionary<string, string>());
+            var xmlObj = ReadXml();
+            var operators = xmlObj.GroupBy(t => t.ОператорId)
+                .Select(t => new Operator { Id = t.First().ОператорId, Name = t.First().Оператор })
+                .ToList();
+            comboBox1.Items.Add(new Operator() { Id = -1, Name = "Все" });
+            comboBox1.Items.AddRange(operators.ToArray());
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServioPumpGAS_Driver.Driver.Service(ReadXml((comboBox1.SelectedItem as Operator).Id)
+                .Select(t => new { t.Id, t.IP }).ToDictionary(i => i.Id.ToString(), t=>t.IP));
+        }
+    }
+}
